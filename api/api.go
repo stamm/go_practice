5c6f873a21053c2bd206f641c0845e535096a1a4
fee1good <vit.kor40@gmail.com>
2018-04-16 01:20:20 +0300

    try 3

--- api/api.go
+++ api/api.go
@@ -5,75 +5,108 @@
 * Я немного запутался в структуре папок - почему у меня тут package main
 * Чем отличаются ковычки "" '' ``
 * что такое mux
+* как генерить айдишники для объектов структур
+* 41 - зачем ставить запятую, если перечисления нет
+* json.NewDecoder что это?
  */
-// package main
-
-// import (
-// 	"io/ioutil"
-// 	"log"
-// 	"net/http"
-// 	"time"
-// )
-
-// var results []byte
-
-// // GetHandler - Get request handler
-// func GetHandler(w http.ResponseWriter, r *http.Request) {
-// 	resp, err := http.Get("/")
-// 	if err != nil {
-// 		// handle error
-// 	}
-// 	body, err := ioutil.ReadAll(resp.Body)
-// 	defer resp.Body.Close()
-// 	if err != nil {
-// 		http.Error(w, "Error converting results to json",
-// 			http.StatusInternalServerError)
-// 	}
-// 	// w.Write(jsonBody)
-// 	println(string(body))
-// }
-
-// func main() {
-// 	results = append(results, time.Now().Format(time.RFC3339)...)
-
-// 	mux := http.NewServeMux()
-// 	mux.HandleFunc("/calcualte", GetHandler)
-
-// 	log.Printf("listening on port %s", ":8000")
-// 	http.ListenAndServe(":8000", mux)
-// }
 
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-// DefaultHandler - test
-func DefaultHandler(w http.ResponseWriter, r *http.Request) {
-	println("test")
-}
+// ID - id of the ride
+var ID int
+
+// GOOGLEAPIKEY is GOOGLEAPIKEY
+const GOOGLEAPIKEY string = "AIzaSyAEb0MBFh70ZZSVW13pRW0mA2ljc9oXSY4"
 
 func main() {
-	http.HandleFunc("/", DefaultHandler)
+	http.HandleFunc("/", GetHandler)
+	http.ListenAndServe(":8000", nil)
+}
 
-	errServer := http.ListenAndServe(":8000", nil)
-	if errServer != nil {
-		panic(errServer)
+// RideData - collecting data of the current trip
+type RideData struct {
+	ID        int    `json:"id"`
+	StartLat  string `json:"start_lat"`
+	EndLat    string `json:"end_lat"`
+	StartLong string `json:"start_long"`
+	EndLong   string `json:"end_long"`
+}
+
+//RideValues - calculate values
+type RideValues struct {
+	ID       string `json:"id"`
+	Distance string `json:"distance"`
+	Duration string `json:"duration"`
+}
+
+type Rows struct {
+	Rows []Elements `json:"elements"`
+}
+
+type Elements struct {
+	Distance Distance `json:"distance"`
+	Duration Duration `json:"duration"`
+}
+
+type Distance struct {
+	Value int `json:"value"`
+}
+
+type Duration struct {
+	Value int `json:"value"`
+}
+
+var rides []RideData
+
+// GetHandler - handle GET params
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	keys := r.URL.Query()
+	if len(keys) < 1 {
+		log.Println("No params passed")
+		return
 	}
 
-	res, err := http.Get("http://localhost:8000")
-	if err != nil {
-		log.Fatal(err)
+	ride := RideData{
+		ID:        ID,
+		StartLat:  r.URL.Query().Get("start_lat"),
+		EndLat:    r.URL.Query().Get("end_lat"),
+		StartLong: r.URL.Query().Get("start_long"),
+		EndLong:   r.URL.Query().Get("end_long"),
 	}
-	content, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
+
+	rideJSON, _ := json.Marshal(ride)
+
+	ID++
+	fmt.Println("LOG: " + string(rideJSON))
+	GetDistanceAndDuration(ride.StartLat, ride.EndLat, ride.StartLong, ride.EndLong)
+}
+
+//GetDistanceAndDuration - get distance and duration from google api
+func GetDistanceAndDuration(slat string, endlat string, slng string, endlng string) {
+	url := "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + slat + "," + slng + "&destinations=" + endlat + "," + endlng + "&key=" + GOOGLEAPIKEY
+	rs, err := http.Get(url)
+	// Process response
 	if err != nil {
-		log.Fatal(err)
+		println("test1")
+		panic(err)
 	}
-	fmt.Printf("%s", content)
-	println("asdasd")
+	defer rs.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var rows Rows
+	json.Unmarshal(bodyBytes, &rows)
+	fmt.Println(rows)
+	fmt.Println(string(bodyBytes))
 }
